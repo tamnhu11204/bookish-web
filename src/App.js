117747235c611d0f575de6fcc0e1b5eb91f772dd
tamnhu11204 <22521053@gm.uit.edu.vn>
2024-10-29 20:11:26 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React from 'react'
-import { Button } from 'antd';
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slides/counterSlide'
+import React, { Fragment } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { routes } from './routes'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
 
  function App() {
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
-
   return (
     <div>
-      <Button type="primary">Primary Button</Button>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <Router>
+        <Routes>
+          {routes.map((route)=> {
+            const Page=route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return (
+              <Route path={route.path} element={<Page/>}/>
+            )
+            })}
+        </Routes>
+      </Router>
     </div>
   )
 }
